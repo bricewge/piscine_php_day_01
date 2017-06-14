4be63dc89757a72e602b304080b716f9bd0d0843
Brice Waegeneire <brice.wge@gmail.com>
2017-06-14 17:52:16 +0200

    ex01 fix, I miss understood the instructions

--- ex01/mlx.php
+++ ex01/mlx.php
@@ -1,17 +1,2 @@
 #!/usr/bin/php
-<?PHP
-
-$limit=80;
-$times=1000;
-$char="X";
-
-while ($times > 0) {
-    if ($times > $limit) {
-        $nb_char = $limit;
-    } else {
-        $nb_char = $times;
-    }
-    echo str_repeat($char, $nb_char)."\n";
-    $times = $times - $nb_char;
-}
-?>
+<?PHP for($i=0;$i<1000;$i++)echo"X";echo"\n";?>
